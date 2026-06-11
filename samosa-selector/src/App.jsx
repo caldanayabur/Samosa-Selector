2b2755e466a2633a42ecfa82694d1f17b4bde2be
Carolina Aldana <caldanayabur@usf.edu>
2026-06-10 21:55:11 -0400

    feat: add upgrade options for samosa click multipliers

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -13,6 +13,19 @@ const App = () => {
       <h1>Samosa Selector</h1>
       <h2>Count: {count}</h2>
       <img className="samosa" src={samosa} onClick={updateCount} />
+      <div className="upgrade">
+        <h3>Double Stuffed 👯‍♀️</h3>
+        <p>2x per click</p>
+        <button>10 samosas</button>
+
+        <h3>Party Pack 🎉</h3>
+        <p>5x per click</p>
+        <button>100 samosas</button>
+
+        <h3>Full Feast 👩🏽‍🍳</h3>
+        <p>10x per click</p>
+        <button>1000 samosas</button>
+      </div>
     </div>
   )
 }
