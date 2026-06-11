4420704300e180ed681c09473219ee3d362e3d51
Carolina Aldana <caldanayabur@usf.edu>
2026-06-10 21:47:13 -0400

    feat: implement count functionality in Samosa Selector

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import samosa from './assets/samosa.png'
+import { useState } from 'react';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <h1>Samosa Selector</h1>
-      <h2>Count:</h2>
-      <img className="samosa" src={samosa} />
+      <h2>Count: {count}</h2>
+      <img className="samosa" src={samosa} onClick={updateCount} />
     </div>
   )
 }
